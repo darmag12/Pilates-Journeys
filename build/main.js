4937e717cb16e43900d07fec84584804bda68980
Daryl Magera <Mageradaryl12@gmail.com>
2024-08-21 20:40:13 -0700

    added logo animation

---
+++ build/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// const logo = document.querySelectorAll('#logo path');
+
+// for (let i = 0; i < logo.length; i++) {
+//     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// }
+
+
+// console.log('Hi');
