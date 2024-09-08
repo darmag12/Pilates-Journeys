01b0ce4e7927540c907607bc13321f466083b482
Daryl Magera <Mageradaryl12@gmail.com>
2024-09-08 05:08:23 -0700

    updated nav links

--- main-nav.php
+++ main-nav.php
@@ -9,12 +9,12 @@
         <a href="javascript:void(0)" class="closebtn" data-closeNav>&times;</a>
         <nav class="topnav overlay-content" data-navItemsContainer>
           <a class="topnav__link" href="<?php echo site_url(); ?>">Home</a>
-          <a class="topnav__link" href="#about">About</a>
-          <a class="topnav__link" href="#pricing">Pricing</a>
-          <a class="topnav__link" href="#classes">Classes</a>
-          <a class="topnav__link" href="#blog">Blog</a>
+          <a class="topnav__link" href="<?php echo site_url(); ?>#about">About</a>
+          <a class="topnav__link" href="<?php echo site_url(); ?>#pricing">Pricing</a>
+          <a class="topnav__link" href="<?php echo site_url(); ?>#classes">Classes</a>
+          <a class="topnav__link" href="<?php echo site_url(); ?>#blog">Blog</a>
           <!-- <a class="topnav__link" href="#">Retreats</a> -->
-          <a class="topnav__link button button--secondary" href="#contact">Contact</a>
+          <a class="topnav__link button button--secondary" href="<?php echo site_url(); ?>#contact">Contact</a>
         </nav>
       </div>
       <div class="openbtn" data-openNav>
